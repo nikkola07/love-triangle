e353fc1b5aa2847fa2494fddf7de17855b9ab706
nikkola07 <nikolayshkuda@mail.ru>
2018-09-13 17:09:13 +0300

    Count love triangles phenomenons

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+  for(i=0; i<preferences.length; i++){
+    if (preferences[i]>[i+1]){
+      var a = preferences[i];
+      var b = preferences[a-1];
+      var c = preferences[b-1];
+      if((c == [i+1]) && (b > [i+1])){
+        count++;
+      }
+    }    
+  }
+  return count;
 };
